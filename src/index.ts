025efec2f1645445d3d06669b9745f06b97ba045
SONIC3D.LaptopHP <sonic3d@gmail.com>
2017-07-23 02:10:55 +0800

    Modify: Improved program entry code.

--- src/index.ts
+++ src/index.ts
@@ -11,22 +11,43 @@ import { GDITrack, GDIDisc, GeneralGDIWriter } from "./gdi-parser";
 
 module app {
     export class MainEntry {
+        public static create(inputGdi: string, outputMode: number, outputDir: string): MainEntry {
+            let retVal: MainEntry = new MainEntry();
+            if ((!retVal) || (!retVal.init(inputGdi, outputMode, outputDir))) {
+                retVal = undefined;
+            }
+            return retVal;
+        }
+
+        protected m_inputGdi: string;
+        protected m_outputDir: string;
+        protected m_outputMode: number;
+
         constructor() {
             console.log("MainEntry created!");
+        }
+
+        public init(inputGdi: string, outputMode: number, outputDir: string): boolean {
+            if (outputMode != 0) {  // 0 for general gdi writer, 1 for redump gdi writer
+                return false;
+            }
+            this.m_inputGdi = inputGdi;
+            this.m_outputDir = outputDir;
+            this.m_outputMode = outputMode;
+            return true;
         }
 
         public exec(): void {
             // TODO: Program entry logic
             console.log("MainEntry::exec()");
-            //let gdiTrack = new GDITrack(123, 4, 2352, "track01.bin", 0);
-            GDIDisc.createFromFile("test.gdi", (gdiLayout:GDIDisc) => {
+            GDIDisc.createFromFile(this.m_inputGdi, (gdiLayout: GDIDisc) => {
                 console.log("GDI file parsing finished.");
 
                 gdiLayout.printInfo();
                 gdiLayout.printIpBinInfo();
                 gdiLayout.unload();
 
-                let writer = GeneralGDIWriter.create(gdiLayout, "./");
+                let writer = GeneralGDIWriter.create(gdiLayout, this.m_outputDir);
                 writer.exec();
             });
         }
@@ -34,6 +55,17 @@ module app {
 }
 
 console.log("Program started!");
-let instance = new app.MainEntry();
-instance.exec();
+let args:string[] = process.argv.slice(2);
+console.log('Commandline arguments: ', args);
+if (args.length != 3) {
+    // TODO: print help
+} else {
+    let inputGdiFile: string = args[0];
+    let outputMode: number = parseInt(args[1]);
+    let outputDir: string = args[2];
+    let instance = app.MainEntry.create(inputGdiFile, outputMode, outputDir);
+    if (instance) {
+        instance.exec();
+    }
+}
 console.log("Program exit!");
