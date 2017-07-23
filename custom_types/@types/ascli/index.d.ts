fa157d4431a716f4ab1c93255d77bed454c25f9d
Lu ZongJing <sonic3d@gmail.com>
2017-07-24 05:30:12 +0800

    Add: Custom @types for ascli

---
+++ custom_types/@types/ascli/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * index.ts
+ * Created on 2017/Jul/24
+ *
+ * Author:
+ *      "ZongJing Lu <sonic3d@gmail.com>"
+ *
+ * Copyright (c) 2017 "ZongJing Lu <sonic3d@gmail.com>"
+ */
+
+export function ascli(name: string): any;
